6593ad2703e21882c5079dfd6da09281561e5621
shagunmistry <shagun.mistry@hotmail.com>
2017-07-17 23:21:26 -0400

    Updated the card which contains videos
    Problems: 
    --Line 34: the Firebase databaseRef.on method is being fired twice. Also, document.appendChild seems to work but Player does not work even when it already has a valid SRC value as an attribute. The commented out video section part in the render() works when you uncomment it.

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -24,6 +24,63 @@ class CardContainer extends Component {
         }
         this.showModal = this.showModal.bind(this);
         this.hideModal = this.hideModal.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
+        this.componentWillMount = this.componentWillMount.bind(this);
+    }
+
+    /**
+     *This will be loaded once this page renders.  Get all the video information and then render player divs based on that. 
+     */
+    componentWillMount() {
+        /**
+       * When the window loads, this will be ran. 
+       * ---------------This is being fired twice. Returns 48 length even though it should return 24. right now. 
+       */
+        var videosRef = dataRef.ref('posts/');
+        videosRef.once('value', function (snapshot) {
+            snapshot.forEach(function (data) {
+                //Get the information of one video.
+                dataValues.push(data.val().challenges);
+                dataValues.push(data.val().dislikes);
+                dataValues.push(data.val().likes);
+                dataValues.push(data.val().userid);
+                dataValues.push(data.val().videoCategory);
+                dataValues.push(data.val().videoDesc);
+                dataValues.push(data.val().videoTitle);
+                dataValues.push(data.val().videoURL);
+                window.alert(data.val().videoURL);
+                //start creating new elements. 
+                //assign all the variables to 
+                //create the row element. 
+                var videoRow = document.createElement("div");
+                videoRow.className = 'col-md-4';
+                videoRow.setAttribute("id", "videoRows");
+                //create the myPlayer div which will contain the Player. 
+                var myPlayerDiv = document.createElement("div");
+                myPlayerDiv.setAttribute("id", "myPlayer");
+                //create my player with the information from the attributes.
+                var videoPlayer = document.createElementNS('', 'Player');
+                videoPlayer.setAttribute("src", data.val().videoURL);
+                myPlayerDiv.appendChild(videoPlayer);
+                videoRow.appendChild(myPlayerDiv);
+                document.getElementById('videoList').appendChild(videoRow);
+            })
+
+        });
+        //Maybe you should erase the array after all the information for a vid is displayed. so that it starts from 0 again. 
+        //Get the div element under which the video will go. 
+        //Create div "videoRows" for every two videos added so that it's in order.
+        /*
+        for (var i = 0; i < dataValues.length; i++) {
+            if (i === 0) {
+               
+            }
+
+        } */
+
+    }
+    componentDidMount() {
+
     }
 
     showModal() {
@@ -34,6 +91,7 @@ class CardContainer extends Component {
         this.refs.modal.hide();
     }
 
+
     /**
      * The way we will set it up is: 
      * Based on the category name, get the download URLS from the DATABASE and set the source of the videos by document.getElementID
@@ -43,33 +101,20 @@ class CardContainer extends Component {
     render() {
 
         function initApp() {
-            var videosRef = dataRef.ref('posts/');
-            videosRef.on('child_added', function (data) {
-                window.alert(data.val());
-               dataValues.push(data.val().challenges);
-               dataValues.push(data.val().dislikes);
-               dataValues.push(data.val().likes);
-               dataValues.push(data.val().userid);
-               dataValues.push(data.val().videoCategory);
-               dataValues.push(data.val().videoDesc);
-               dataValues.push(data.val().videoTitle);
-               dataValues.push(data.val().videoURL);
-            })
+
         }
-        //load the initApp that checks user status on page load
+
         window.addEventListener('load', function () {
             initApp()
         });
-
-
 
         return (
             <div>
                 <div className="card">
                     <h4 className="card-header" ><a href="index.html">{this.props.categoryName}</a></h4>
                     <div className="card-text">
-                        <div className="row" >
-                            <div className="col-md-4">
+                        <div className="row" id="videoList" >
+                            {/*      <div className="col-md-4" id="videoRows">
                                 <div id="myPlayer" onClick={() => this.showModal()}>
                                     <Player poster=""
                                         src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
@@ -81,9 +126,8 @@ class CardContainer extends Component {
                                         </ControlBar>
                                         <BigPlayButton position="center" />
                                     </Player>
-                                    <a href="http://localhost:3000/Profilepage/X6aAutkrbAXAEZdyAe7m5NLXmuj1">User Profile</a>
                                 </div>
-                                <div id="myPlayer" >
+                                <div id="myPlayer" onClick={() => this.showModal()}>
                                     <Player poster=""
                                         src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
                                         <ControlBar>
@@ -95,59 +139,7 @@ class CardContainer extends Component {
                                         <BigPlayButton position="center" />
                                     </Player>
                                 </div>
-                            </div>
-                            <div className="col-md-4">
-                                <div id="myPlayer" >
-                                    <Player poster=""
-                                        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
-                                        <ControlBar>
-                                            <ForwardControl seconds={30} order={1.2} />
-                                            <CurrentTimeDisplay order={4.1} />
-                                            <TimeDivider order={4.2} />
-                                            <VolumeMenuButton enabled />
-                                        </ControlBar>
-                                        <BigPlayButton position="center" />
-                                    </Player>
-                                </div>
-                                <div id="myPlayer" >
-                                    <Player poster=""
-                                        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
-                                        <ControlBar>
-                                            <ForwardControl seconds={30} order={1.2} />
-                                            <CurrentTimeDisplay order={4.1} />
-                                            <TimeDivider order={4.2} />
-                                            <VolumeMenuButton enabled />
-                                        </ControlBar>
-                                        <BigPlayButton position="center" />
-                                    </Player>
-                                </div>
-                            </div>
-                            <div className="col-md-4">
-                                <div id="myPlayer" >
-                                    <Player poster=""
-                                        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
-                                        <ControlBar>
-                                            <ForwardControl seconds={30} order={1.2} />
-                                            <CurrentTimeDisplay order={4.1} />
-                                            <TimeDivider order={4.2} />
-                                            <VolumeMenuButton enabled />
-                                        </ControlBar>
-                                        <BigPlayButton position="center" />
-                                    </Player>
-                                </div>
-                                <div id="myPlayer" >
-                                    <Player poster=""
-                                        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
-                                        <ControlBar>
-                                            <ForwardControl seconds={30} order={1.2} />
-                                            <CurrentTimeDisplay order={4.1} />
-                                            <TimeDivider order={4.2} />
-                                            <VolumeMenuButton enabled />
-                                        </ControlBar>
-                                        <BigPlayButton position="center" />
-                                    </Player>
-                                </div>
-                            </div>
+                            </div> */}
                             <p className="card-text"><a href={"http://localhost:3000/" + this.props.categoryName}>More...</a></p>
                         </div>
                     </div>
