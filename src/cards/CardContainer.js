ba9f4fc0f07e1515ca17b5df35ba604e603d4a70
shagunmistry <shagun.mistry@hotmail.com>
2017-08-19 23:08:32 -0400

    Video Update
    Able to show the videos from the database now. Although, it still runs twice unless executed from a button

--- src/cards/CardContainer.js
+++ src/cards/CardContainer.js
@@ -64,15 +64,11 @@ class CardContainer extends Component {
                 //window.alert(data.val().videoURL);
 
                 //start creating new elements. Assign all the variables to create the row element. 
-                let videoPlace = document.getElementById('videoText');
-                var videoLine = document.createElement('div');
-                videoLine.setAttribute('id', 'singleVideoContainer');
-                //creates a video element. Player could not be created.
-                //var videoPlyer = document.createElementNS('', 'video');
-                /*videoPlyer.setAttribute('src', data.val().videoURL);
-                videoPlyer.autoplay = true;
-                videoLine.appendChild(videoPlyer);*/
-                
+                let videoText = document.getElementById('videoText');
+                var singlevideoConainer = document.createElement('div');
+                singlevideoConainer.setAttribute('id', 'singleVideoContainer');
+                //videoPlace --> videoLine --> videoInfoSecDiv --> videoInfoSecRow/Desc/Pic
+
                 //set up the profile information. 
                 var videoInfoSecDiv = document.createElement('div');
                 videoInfoSecDiv.setAttribute('id', 'videoInfoSection');
@@ -90,18 +86,27 @@ class CardContainer extends Component {
                 var videoInfoDesc = document.createElement('div');
                 videoInfoDesc.setAttribute('className', 'col-md-11');
                 var videoInfoDescPlace = document.createElement('div');
-                videoInfoDescPlace.setAttribute('className','generalVideoInfo');
+                videoInfoDescPlace.setAttribute('className', 'generalVideoInfo');
                 videoInfoDescPlace.innerText = data.val().videoTitle;
+
                 videoInfoDesc.appendChild(videoInfoDescPlace);
                 //Attach both of those divs to Row Div then to videoInfoSecDiv
                 videoInfoSecRow.appendChild(videoInfoPic);
                 videoInfoSecRow.appendChild(videoInfoDesc);
                 videoInfoSecDiv.appendChild(videoInfoSecRow);
 
-                videoLine.appendChild(videoInfoSecDiv);
-                videoPlace.appendChild(videoLine);    
-                
-                //videoPlace --> videoLine --> videoInfoSecDiv --> videoInfoSecRow/Desc/Pic
+                //create a video player, attach the src to it, format the css for it and the append it to singleVideoContainer. 
+                var videoPlayer = document.createElement("VIDEO");
+                videoPlayer.setAttribute("src", data.val().videoURL);
+                videoPlayer.setAttribute("id", "videoPlayerStyle");
+                videoPlayer.setAttribute("controls", "controls");
+
+
+                //append the video's information box to the singleVideoContainer Div and then append the video to it as well.
+                singlevideoConainer.appendChild(videoPlayer);
+                singlevideoConainer.appendChild(videoInfoSecDiv);
+                videoText.appendChild(singlevideoConainer);
+
             })
         });
     }
@@ -116,7 +121,74 @@ class CardContainer extends Component {
     render() {
 
         function initApp() {
+            var dataValues = [];
+            /**
+           * When the window loads, this will be ran. 
+           * ---------------This is being fired twice. Returns 48 length even though it should return 24. right now. 
+           */
+            var videosRef = dataRef.ref('posts/');
+            videosRef.once('value', function (snapshot) {
+                snapshot.forEach(function (data) {
+                    //Get the information of one video.
+                    dataValues.push(data.val().challenges);
+                    dataValues.push(data.val().dislikes);
+                    dataValues.push(data.val().likes);
+                    dataValues.push(data.val().userid);
+                    dataValues.push(data.val().videoCategory);
+                    dataValues.push(data.val().videoDesc);
+                    dataValues.push(data.val().videoTitle);
+                    dataValues.push(data.val().videoURL);
 
+                    //this returns the true value (even though it's twice)
+                    //window.alert(data.val().videoURL);
+
+                    //start creating new elements. Assign all the variables to create the row element. 
+                    let videoText = document.getElementById('videoText');
+                    var singlevideoConainer = document.createElement('div');
+                    singlevideoConainer.setAttribute('id', 'singleVideoContainer');
+                    //videoPlace --> videoLine --> videoInfoSecDiv --> videoInfoSecRow/Desc/Pic
+
+                    //set up the profile information. 
+                    var videoInfoSecDiv = document.createElement('div');
+                    videoInfoSecDiv.setAttribute('id', 'videoInfoSection');
+                    //The Row to the div
+                    var videoInfoSecRow = document.createElement('div');
+                    videoInfoSecRow.setAttribute('className', 'row');
+                    //Col-md-1 for the pic
+                    var videoInfoPic = document.createElement('div');
+                    videoInfoPic.setAttribute('className', 'col-md-1');
+                    var videoInfoPicPlace = document.createElement('img');
+                    videoInfoPicPlace.className = "videoInfoPic";
+                    videoInfoPicPlace.src = "https://firebasestorage.googleapis.com/v0/b/challengemetest-ea2e0.appspot.com/o/selfie1.PNG?alt=media&token=807a3afa-e4dc-4020-87ed-25219c305732";
+                    videoInfoPic.appendChild(videoInfoPicPlace);
+                    //col-md-11 for the video title and other information
+                    var videoInfoDesc = document.createElement('div');
+                    videoInfoDesc.setAttribute('className', 'col-md-11');
+                    videoInfoDesc.setAttribute("id", "descriptionId");
+                    var videoInfoDescPlace = document.createElement('div');
+                    videoInfoDescPlace.setAttribute("id", "generalVideoInfo");
+                    videoInfoDescPlace.innerHTML = data.val().videoTitle + "<br/>" + data.val().videoDesc;
+
+                    videoInfoDesc.appendChild(videoInfoDescPlace);
+                    //Attach both of those divs to Row Div then to videoInfoSecDiv
+                    videoInfoSecRow.appendChild(videoInfoPic);
+                    videoInfoSecRow.appendChild(videoInfoDesc);
+                    videoInfoSecDiv.appendChild(videoInfoSecRow);
+
+                    //create a video player, attach the src to it, format the css for it and the append it to singleVideoContainer. 
+                    var videoPlayer = document.createElement("VIDEO");
+                    videoPlayer.setAttribute("src", data.val().videoURL);
+                    videoPlayer.setAttribute("id", "videoPlayerStyle");
+                    videoPlayer.setAttribute("controls", "controls");
+
+
+                    //append the video's information box to the singleVideoContainer Div and then append the video to it as well.
+                    singlevideoConainer.appendChild(videoPlayer);
+                    singlevideoConainer.appendChild(videoInfoSecDiv);
+                    videoText.appendChild(singlevideoConainer);
+
+                })
+            });
         }
 
         window.addEventListener('load', function () {
@@ -161,9 +233,9 @@ class CardContainer extends Component {
                                 </div>
                             </div>
                         </div> */}
-                        <button className="btn btn-danger btn-4" onClick={() => this.showVideos()}>Show Videos</button>
-                        <p className="card-text"><a href={"http://www.beztbaba.com/" + this.props.categoryName}>More...</a></p>
                     </div>
+                    { /*  <button className="btn btn-danger btn-4" onClick={() => this.showVideos()}>Show Videos</button>*/}
+                    <p className="card-text"><a href={"http://www.beztbaba.com/" + this.props.categoryName}>More...</a></p>
                 </div>
                 {/*    <Modal ref="modal">
                     <div id="compVideo">
